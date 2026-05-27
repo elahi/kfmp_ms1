a4a6c1ee4e9ec4fab24feb9277e83a5cf6d40b29
elahi <robin.elahi@gmail.com>
2026-05-26 17:17:24 -0700

    updated processing script for benthic and kelp view separate

---
+++ kfmp_transect_video_species/_process_raw_data_lump_ms1.R
@@ -0,0 +1,128 @@
+################################################################################
+##' @title Process raw data, lump to ms1 categories
+##' @author Robin Elahi
+##' @date 2026-05-26
+##' @log 
+################################################################################
+
+#### File paths ####
+here::i_am("kfmp_transect_video_species/_process_raw_data_lump_ms1.R")
+library(here)
+folder <- "kfmp_transect_video_species"
+file_name <- "_process_raw_data_lump_ms1.R"
+
+##### PACKAGES, DATA #####
+library(tidyverse)
+library(readxl)
+source(here("R", "ggplot_settings.R"))
+
+# Load raw data from video analysis
+dat <- read_excel("data/kfmp_transect_video_species_260526.xlsx", na = "NA") 
+glimpse(dat)
+head(dat)
+names(dat)
+
+# Load video metadata
+dat_meta <- read_excel("data/kfmp_transect_video_species_260526.xlsx", 
+                       sheet = "metadata", na = "NA") 
+glimpse(dat_meta)
+
+##### WRANGLE DATA #####
+
+# Remove taxa
+dat <- dat |> 
+  filter(ms1 == "yes")
+
+# Create new taxon column, lumping species
+# lump_ms1
+dat <- dat |> 
+  mutate(taxon = ifelse(is.na(lump_ms1), taxon, lump_ms1))
+glimpse(dat)
+
+lump_presence <- function(x)  {ifelse(x == 0, 0, 1)}
+x <- c(0, 0, 1, 3, 2)
+lump_presence(x)
+
+# Summarize to new taxa
+dat <- dat |> 
+  group_by(taxon, phylum, trophic_level, view) |> 
+  summarise(
+    across(.cols = `bird_rock_2023-08-30_t1_k`:`hms110_2025-09-11_t1_b`, 
+           .fns = sum)
+  ) |> 
+  ungroup() |> 
+  arrange(phylum, taxon)
+
+# dat |> filter(taxon == "red_aca_fg") |> View()
+
+# Change to presence-absence
+dat <- dat |> 
+  mutate(across(.cols = `bird_rock_2023-08-30_t1_k`:`hms110_2025-09-11_t1_b`, 
+                .fns = lump_presence))
+  
+# Separate into benthic vs kelp datasets
+dat_k <- dat |> 
+  filter(view == "kelp")
+
+dat_b <- dat |> 
+  filter(view == "benthic")
+
+##### MAKE VIDEO x SPECIES MATRIX - KELP #####
+dat_k_wide <- dat_k |> 
+  select(-c(phylum:view)) |> 
+  tibble::column_to_rownames(var = "taxon") %>% # Move 'Type' column to row names
+  t() |> 
+  as.data.frame() |> 
+  tibble::rownames_to_column(var = "video") |> 
+  tibble() 
+
+dat_k_wide2 <- dat_meta |> 
+  filter(view == "kelp") |> 
+  left_join(dat_k_wide, by = "video")
+
+d_wide <- dat_k_wide2
+
+# Remove numeric columns that contain values >= 0
+d_sub <- d_wide %>%
+  select(where(~ !is.numeric(.) || any(. > 0)))
+
+# Create matrix (species x sample matrix) metadata
+names(d_sub)
+m_meta <- d_sub %>% select(video:sand)
+m <- d_sub %>% select(-c(video:sand))
+m_meta
+m
+
+# Rename
+kelp_meta <- m_meta
+kelp_m <- m
+
+##### MAKE VIDEO x SPECIES MATRIX - BENTHIC #####
+dat_b_wide <- dat_b |> 
+  select(-c(phylum:view)) |> 
+  tibble::column_to_rownames(var = "taxon") %>% # Move 'Type' column to row names
+  t() |> 
+  as.data.frame() |> 
+  tibble::rownames_to_column(var = "video") |> 
+  tibble() 
+
+dat_b_wide2 <- dat_meta |> 
+  filter(view == "benthic") |> 
+  left_join(dat_b_wide, by = "video")
+
+d_wide <- dat_b_wide2
+
+# Remove numeric columns that contain values >= 0
+d_sub <- d_wide %>%
+  select(where(~ !is.numeric(.) || any(. > 0)))
+
+# Create matrix (species x sample matrix) metadata
+names(d_sub)
+m_meta <- d_sub %>% select(video:sand)
+m <- d_sub %>% select(-c(video:sand))
+m_meta
+m
+
+# Rename
+benthic_meta <- m_meta
+benthic_m <- m
