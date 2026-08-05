49c45ac5f24d5079665b7a3da4c928eebd785d26
elahi <robin.elahi@gmail.com>
2026-08-04 17:05:14 -0700

    updated plot and stats for behavior

--- fig_urchin_class_exercise/fig_urchin_class_exercise.R
+++ fig_urchin_class_exercise/fig_urchin_class_exercise.R
@@ -113,7 +113,6 @@ summary(fit3)
 
 AIC(fit, fit2, fit3)
 
-
 ##### TEST BEHAVIORAL HYPOTHESIS #####
 
 ## Summarize means per video segment
@@ -146,3 +145,105 @@ means_summary |>
 
 ggsave(paste(folder, "/figs/", file_name, "_b.pdf", sep = ""), height = 3, width = 7)
 ggsave(paste(folder, "/figs/", file_name, "_b.jpg", sep = ""), height = 3, width = 7)
+
+## Log-scale
+min_mean <- min(dl_obs_means$obs_mean[dl_obs_means$obs_mean != 0])
+dl_obs_means <- dl_obs_means |> 
+  mutate(log_mean = log(obs_mean + min_mean))
+
+## Summarize means across video segment observer means
+log_means_summary <- dl_obs_means |> 
+  ungroup() |> 
+  group_by(Forested, Urchin, Behavior) %>% 
+  summarise(mean = mean(log_mean), 
+            sd = sd(log_mean), 
+            n = n(), 
+            se = sd / n, 
+            ci = 1.96*se)
+
+## Plot
+behavior_cols <- c("orange", "#646464")
+log_means_summary |> 
+  ggplot(aes(Forested, mean, fill = Behavior, shape = Behavior, group = Behavior)) + 
+  geom_errorbar(aes(ymin = mean - ci, ymax = mean + ci, color = Behavior), width = 0.05) + 
+  geom_line(aes(color = Behavior)) + 
+  geom_point(alpha = 1, size = 3) +
+  scale_fill_manual(values = behavior_cols) + 
+  scale_color_manual(values = behavior_cols) + 
+  scale_shape_manual(values = c(21, 22)) +
+  facet_wrap(~ Urchin, scales = "free_y") +
+  labs(x = "Site", y = "Mean log count per video segment")
+
+ggsave(paste(folder, "/figs/", file_name, "_c.pdf", sep = ""), height = 3, width = 7)
+ggsave(paste(folder, "/figs/", file_name, "_c.jpg", sep = ""), height = 3, width = 7)
+
+## Back-transformed plot
+log_means_summary <- log_means_summary |> 
+  mutate(bt_mean = exp(mean) - min_mean, 
+         bt_lower = exp(mean - 1.96 * se) - min_mean, 
+         bt_upper = exp(mean + 1.96 * se) - min_mean)
+
+## Plot back-transformed
+behavior_cols <- c("orange", "#646464")
+log_means_summary |> 
+  ggplot(aes(Forested, bt_mean, fill = Behavior, shape = Behavior, group = Behavior)) + 
+  geom_errorbar(aes(ymin = bt_lower, ymax = bt_upper, color = Behavior), width = 0.05) + 
+  geom_line(aes(color = Behavior)) + 
+  geom_point(alpha = 1, size = 3) +
+  scale_fill_manual(values = behavior_cols) + 
+  scale_color_manual(values = behavior_cols) + 
+  scale_shape_manual(values = c(21, 22)) +
+  facet_wrap(~ Urchin, scales = "free_y") +
+  labs(x = "Site", y = "Mean count per video segment")
+
+ggsave(paste(folder, "/figs/", file_name, "_d.pdf", sep = ""), height = 3, width = 7)
+ggsave(paste(folder, "/figs/", file_name, "_d.jpg", sep = ""), height = 3, width = 7)
+
+## ANOVA - natural (residuals are not homogeneous)
+m1 <- lm(obs_mean ~ Behavior * Forested * Urchin, data = dl_obs_means)
+anova(m1)
+plot(m1)
+
+## ANOVA - log (much better)
+m1 <- lm(log_mean ~ Behavior * Forested * Urchin, data = dl_obs_means)
+anova(m1)
+plot(m1)
+
+anova_table <- tidy(aov(m1))
+anova_table
+
+## Formatted
+library(kableExtra)
+m1 <- aov(log_mean ~ Behavior * Forested * Urchin, data = dl_obs_means)
+summary(m1)
+
+# Format the table
+formatted_table <- anova_table %>%
+  as.data.frame() %>%
+  tibble::rownames_to_column("Source") %>%
+  rename(
+    df = df,
+    SS = `sumsq`,
+    MS = `meansq`,
+    F = `statistic`,
+    p = `p.value`
+  ) %>%
+  mutate(
+    # Format p-values
+    p = case_when(
+      p < 0.001 ~ "< 0.001",
+      p < 0.01 ~ sprintf("%.3f", p),
+      TRUE ~ sprintf("%.3f", p)
+    ),
+    # Round other values
+    SS = round(SS, 2),
+    MS = round(MS, 2),
+    F = round(F, 2)
+  )
+
+# View in console
+print(formatted_table)
+
+# Export to CSV
+write.csv(formatted_table, paste(folder, "/figs/", file_name, ".csv", sep = ""), row.names = FALSE)
+
